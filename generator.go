6a1a6a02e5baf2c1ae1ffd5dea53547662e80ff9
Brandon B <lordnynex@gmail.com>
2016-08-04 17:06:48 -0700

    yet more debugging

--- generator.go
+++ generator.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/sugyan/ttyread"
 	"image/gif"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"time"
+
+	"github.com/sugyan/ttyread"
 )
 
 // GifGenerator type
@@ -25,6 +27,7 @@ func NewGifGenerator() *GifGenerator {
 func (g *GifGenerator) Generate(inFile string, outFile string) (err error) {
 	tempDir, err := ioutil.TempDir("", "ttygif")
 	if err != nil {
+		log.Println("FNR: 28")
 		return
 	}
 	defer os.RemoveAll(tempDir)
@@ -47,12 +50,14 @@ func (g *GifGenerator) Generate(inFile string, outFile string) (err error) {
 		imgPath := filepath.Join(tempDir, fmt.Sprintf("%03d_%06d", timestamp.Sec, timestamp.Usec))
 		fileType, err := CaptureImage(imgPath)
 		if err != nil {
+			log.Println("FNR: 53")
 			return
 		}
 		worker.AddTargetFile(imgPath, fileType)
 		return nil
 	})
 	if err != nil {
+		log.Println("FNR: 60")
 		return
 	}
 	// get paletted images from capture files
@@ -71,6 +76,7 @@ func (g *GifGenerator) Generate(inFile string, outFile string) (err error) {
 	}()
 	images, err := worker.GetAllImages()
 	if err != nil {
+		log.Println("FNR: 79")
 		return
 	}
 	close(progress)
@@ -78,6 +84,7 @@ func (g *GifGenerator) Generate(inFile string, outFile string) (err error) {
 	// generate GIF file
 	file, err := os.Create(outFile)
 	if err != nil {
+		log.Println("FNR: 87")
 		return
 	}
 	defer file.Close()
@@ -90,6 +97,7 @@ func (g *GifGenerator) Generate(inFile string, outFile string) (err error) {
 	}
 	err = gif.EncodeAll(file, &opts)
 	if err != nil {
+		log.Println("FNR: 100")
 		return
 	}
 	return nil
